5c728fe87dad4e8617012cfa59a364681a3ddd52
FernandaFialho <52725043+FernandaFialho@users.noreply.github.com>
2019-11-09 02:01:53 -0300

    Add files via upload

---
+++ MonsterRPG 1.0/src/monsterRPG/sistema/monstro/DragaoVermelho.java
@@ -0,0 +1,31 @@
+package monsterRPG.sistema.monstro;
+
+public class DragaoVermelho extends Monstro{
+	public DragaoVermelho(String nome, String historia, String tamanho, int vida, int defesa, int velocidade, int forca, int destreza, int constituiçao, int inteligencia, int sabedoria, int carisma, double nivel) {
+		super(nome, historia, tamanho, vida, defesa, velocidade, forca, destreza, constituiçao, inteligencia, sabedoria, carisma, nivel);
+		this.preencherHabilidades();
+		this.preencherImunidades();
+		this.preencherLinguas();
+		this.preencherSentidos();
+	}
+
+	
+	private void preencherHabilidades() {
+		this.getHabilidades().add(new Habilidades("Sopro elemental", "O dragão exala fogo em um cone de 15 pés. Cada criatura nessa área precisa fazer um teste de resistência de dificuldade 13, tomando 7d6 de dano ácido numa falha, ou metade do dano num sucesso."));
+
+	}
+	
+	private void preencherLinguas() {
+		this.getLinguas().add("Draconic");
+	}
+	
+	private void preencherSentidos() {
+		this.getSentidos().add("Visão noturna (60 pés)");
+		this.getSentidos().add("Visão cega (10 pés)");
+
+	}
+	
+	private void preencherImunidades() {
+		this.getImunidades().add("Fogo");
+	}
+}
