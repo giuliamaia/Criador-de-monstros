828c0681678f387f30255d97863ca3e3e505b77b
MrSalamander <51835611+MrSalamander@users.noreply.github.com>
2019-11-09 00:26:17 -0200

    Testando botões

---
+++ MonsterRPG/src/monsterRPG/gui/menu/controller/MenuPrincipalPaneController.java
@@ -0,0 +1,8 @@
+package monsterRPG.gui.menu.controller;
+
+public class MenuPrincipalPaneController {
+	
+	public void Chora() {
+		System.out.println("E CHOROU");
+	}
+}
---
+++ MonsterRPG/src/monsterRPG/sistema/dados/RepositorioMonstros.java
@@ -0,0 +1,8 @@
+package monsterRPG.sistema.dados;
+
+import java.util.List;
+import monsterRPG.sistema.monstro.Monstro;
+
+public class RepositorioMonstros {
+	List<Monstro> monstros;
+}
