40e75ab970d89b289a088247a89102a525b0855b
MrSalamander <51835611+MrSalamander@users.noreply.github.com>
2019-11-09 23:48:37 -0200

    Editei a tela de menu e adicionei uma prévia das funções no controlador

--- MonsterRPG2/src/monsterRPG/gui/menu/controller/MenuPrincipalPaneController.java
+++ MonsterRPG2/src/monsterRPG/gui/menu/controller/MenuPrincipalPaneController.java
@@ -1,8 +1,48 @@
 package monsterRPG.gui.menu.controller;
 
 public class MenuPrincipalPaneController {
+	private boolean favorito = false;
 	
-	public void Chora() {
+	public void Pesquisar() {
 		System.out.println("Funciona para K+1");
+		//TODO Pesquisar
+	}
+	public boolean isBotaoFavoritoPressed() {
+		favorito = !favorito;
+		System.out.println(favorito);
+		return favorito;
+	}
+	public void ordenarPorDataCrescente() {
+		//TODO ordenar
+	}
+	public void ordenarPorDataDecrescente() {
+		//TODO ordenar
+	}
+	public void ordenarPorNomeCrescente() {
+		//TODO ordenar
+	}
+	public void ordenarPorNomeDecrescente() {
+		//TODO ordenar
+	}
+	public void ordenarPorMortos() {
+		//TODO ordenar
+	}
+	public void ordenarPorFavoritos() {
+		//TODO ordenar
+	}
+	public boolean addCriatura() {
+		//TODO adicionar
+		return true;
+	}
+	public boolean removeCriatura() {
+		//TODO remover
+		return true;
+	}
+	public boolean editCriatura() {
+		//TODO editar
+		return true;
+	}
+	public void atualizarLista() {
+		//TODO atualizar
 	}
 }
